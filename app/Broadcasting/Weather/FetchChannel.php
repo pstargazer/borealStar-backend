a65e20a6b2fcf94472659eb719fdd4915c025151
niconicotin <shevvg2004@yandex.ru>
2024-06-08 08:19:47 +0300

    WSS preparations

---
+++ app/Broadcasting/Weather/FetchChannel.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Broadcasting\Weather;
+
+use App\Models\User;
+
+class FetchChannel
+{
+    /**
+     * Create a new channel instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Authenticate the user's access to the channel.
+     */
+    public function join(User $user): array|bool
+    {
+        //
+    }
+}
